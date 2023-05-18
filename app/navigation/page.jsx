4ecfcaa472deedeffc05ca862b8067f09e340522
vinay1668 <feynman.eth2@gmail.com>
2023-05-19 05:25:31 +0530

    added new page

--- app/navigation/page.jsx
+++ app/navigation/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function page() {
   return (
     <div>
-      <h2 className="text-blue-6000">PageSwap</h2>
+      <h2 className="text-blue-6000">Navigation Page!</h2>
     </div>
   );
 }
